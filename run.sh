e0dbcb9744413ec3bdfeb79d41255f90aceb033b
Will <yh@fir.im>
2017-04-05 18:23:44 +0800

    修改

--- run.sh
+++ run.sh
@@ -11,5 +11,10 @@
 
 cd $FLOW_CURRENT_PROJECT_PATH
 
+if [[ ! -f ./pom.xml ]]; then
+  echo -e "${ANSI_RED}No such file ./pom.xml!${ANSI_RESET}"
+  flow_terminate 2
+fi
+
 flow_cmd "mvn test -B" --echo --assert
 flow_result $?
